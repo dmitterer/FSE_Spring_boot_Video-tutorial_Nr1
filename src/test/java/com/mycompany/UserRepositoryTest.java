f3b52da56502c21ae78bb8fae1949378ccfed96e
Dominik Mitterer <dmitterer@tsn.at>
2021-10-27 14:51:21 +0200

    Schreiben vom ersten Test (testAddNew) einem drittes Beispiel (Die Doku befindet sich in der Word Datei beim Punkt 4.7.3)

--- src/test/java/com/mycompany/UserRepositoryTest.java
+++ src/test/java/com/mycompany/UserRepositoryTest.java
@@ -19,10 +19,10 @@ public class UserRepositoryTest {
     @Test
     public void testAddNew(){
         User user = new User();
-        user.setEmail("jack.miller@gmx.net");
-        user.setPassword("jack6368w22");
-        user.setFirstName("Jack");
-        user.setLastName("Miller");
+        user.setEmail("Josef.mayer@aon.at");
+        user.setPassword("jo2921sef");
+        user.setFirstName("Josef");
+        user.setLastName("Mayer");
 
        User savedUser = repo.save(user);
 
