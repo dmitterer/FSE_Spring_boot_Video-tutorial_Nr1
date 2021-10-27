fd030e6b239c81172db749cf19f3808332927495
Dominik Mitterer <dmitterer@tsn.at>
2021-10-27 14:17:37 +0200

    Schreiben vom ersten Test (testAddNew) einem Beispiel (Die Doku befindet sich in der Word Datei beim Punkt 4.7.1)

--- src/test/java/com/mycompany/UserRepositoryTest.java
+++ src/test/java/com/mycompany/UserRepositoryTest.java
@@ -1,4 +1,32 @@
 package com.mycompany;
 
+import com.mycompany.user.User;
+import com.mycompany.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 public class UserRepositoryTest {
+
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+        user.setEmail("domi.mit98@gmail.com");
+        user.setPassword("IloveCoding123");
+        user.setFirstName("Dominik");
+        user.setLastName("Mitterer");
+
+       User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
 }
