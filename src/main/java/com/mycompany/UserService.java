056a36ab3130392fd895fa9234f12b42583dc7cc
Dominik Mitterer <dmitterer@tsn.at>
2021-10-28 18:50:15 +0200

    Erstellen der Klasse "UserNotFoundException" mit einer Methoden "UserNotFoundException". Ergänzungen in den Klassen "UserService.java","UserController.java", "user_form.html" und users.html". Klasse "UserService": Implementierung der Methode "get mit UserNotFoundException". Klasse "UserController": Einfügen vom Model für den Seitentitel und Implementierung der Methode "ShowEditForm". Klasse "user_form.html": Verwendung von [[${PageTile}] und Verstecken von der Id. Klasse "users.html": Ergänzung der Tabellenüberschrift "Enable" und den Werten. (Weitere Doku siehe Word Datei Unterpunkte 4.11.1 bis 4.11.5)

--- src/main/java/com/mycompany/UserService.java
+++ src/main/java/com/mycompany/UserService.java
@@ -1,11 +1,14 @@
 package com.mycompany;
 
 import com.mycompany.user.User;
+import com.mycompany.user.UserNotFoundException;
 import com.mycompany.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +22,18 @@ public class UserService {
     public void save(User user) {
 
         repo.save(user);
+    }
+
+    public  User get(Integer id) throws UserNotFoundException {
+
+        Optional<User> result = repo.findById(id);
+
+        if (result.isPresent()){
+            return  result.get();
+
+        }
+        throw new UserNotFoundException("Could not find any users with ID" + id);
+
+
     }
 }
--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @Controller
@@ -27,6 +29,7 @@ public class UserController {
 
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
        return "user_form";
     }
 
@@ -39,4 +42,21 @@ public class UserController {
         return "redirect:/users";
 
     }
+
+    @GetMapping("/users/edit/{id}")
+
+    public String ShowEditForm(@PathVariable("id") Integer id, Model model,  RedirectAttributes ra){
+        try {
+           User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id +  ")");
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", "The user has been saved successfully.");
+
+            return "redirect:/users";
+        }
+
+    }
+
 }
